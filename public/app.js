07f9e0cb7a222f93348e744d08c1dfd7c68c2c0f
arturmh <52570589+ArturM4@users.noreply.github.com>
2023-01-31 12:12:18 +0100

    Afegir mapa Leaflet

--- public/app.js
+++ public/app.js
@@ -1,3 +1,10 @@
+
+var map = L.map('map').setView([41.483022995325, 2.2685480117798], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
 fetch("get_data.php")
   .then((response) => {
     if (!response.ok) {
@@ -6,5 +13,23 @@ fetch("get_data.php")
     return response.json();
   })
   .then((data) => {
-    console.log(data)
+    geojson = {
+      type: "FeatureCollection",
+      features: [],
+    };
+
+    for (i = 0; i < data.length; i++) {
+      geojson.features.push({
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [data[i].COORD_LON, data[i].COORD_LAT]
+        },
+        "properties": data[i]
+      });
+    }
+
+    //de moment mostra de 100 marcadors, no pot carregar 13k.
+    const slicedArray = geojson.features.slice(0, 100);
+    L.geoJSON(slicedArray).addTo(map);
   })
